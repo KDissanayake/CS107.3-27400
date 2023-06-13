f053039e70992e999e7c94416785d9ca45eb19d7
KDissanayake <118176408+KDissanayake@users.noreply.github.com>
2023-06-13 11:14:03 +0530

    Add files via upload

---
+++ Practical1/C# Tutorial 01/main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+////write a code to input two numbers and get the sum
+//int main()
+//{
+//    int no1,no2,sum;
+//    printf("Enter two numbers");
+//    scanf("%d %d",&no1,&no2);
+//    sum=no1+no2;
+//    printf("The total is %d",sum);
+//
+//}
+
+//// get area of the square
+//int main()
+//{
+//    int length,width,area;
+//    printf("Enter the length");
+//    scanf("%d",&length);
+//    printf("Enter the width");
+//    scanf("%d",&width);
+//
+//    area=length*width;
+//
+//    printf("The area is %d\n",area);
+//
+//}
+
+////enter 5 numbers and calculate the average of five numbers
+//int main()
+//{
+//    int nums[5],sum=0;
+//    float avg;
+//
+//    printf("Enter five numbers\n");
+//
+//    for(int i=0;i<5;i++)
+//    {
+//        scanf("%d",&nums[i]);
+//        sum+=nums[i];
+//    }
+//    avg=(float)sum/5;
+//    printf("The average of the five numbers is %.2f",avg);
+//
+//}
+
+////Write a function that accepts 2 whole numbers as parameters and calculateand return the product
+//int calcSum(int no1,int no2)
+//{
+//    int sum=no1+no2;
+//    return sum;
+//}
+//int  main()
+//{
+//    int no1,no2,sum;
+//
+//    printf("Enter two whole numbers ");
+//    scanf("%d %d",&no1,&no2);
+//
+//    sum=calcSum(no1,no2);
+//    printf("The total is %d",sum);
+//
+//}
+
+//void sum()
+//{
+//    int no1,no2,sum;
+//    printf("Enter two numbers ");
+//    scanf("%d %d",&no1,&no2);
+//
+//    sum = no1+no2;
+//
+//    printf("The total is %d\n",sum);
+//
+//}
+//int main()
+//{
+//    sum();
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
