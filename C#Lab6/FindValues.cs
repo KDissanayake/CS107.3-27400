dd5e88d27cae13d6356bbb94229e1793dd0e379f
KDissanayake <118176408+KDissanayake@users.noreply.github.com>
2023-07-31 01:51:38 +0530

    Add files via upload

---
+++ C#Lab6/FindValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Lab6
+{
+    internal class FindValues
+    {
+        public double FindArea(double radius) 
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+
+        public double FindCir(double radius)
+        {
+            double cir = 2 * Math.PI * radius;
+            return cir;
+        }
+    }
+}
---
+++ C#Lab6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Lab6
+{
+    internal class Program
+    {
+        //create a c# console application to find the area and the circumferance of a circle. user should insert the raduis value to the program.program should contain a separate class call "FindValues". Inside the separate class add two methods call findArea and findCircumference. Both these methods are methods which takes parameters. As the parameter you should pass the radius value. By using above two methods find the area and circumference of the circle and return the answer from both methods. Create a class object in main class and call both methods and display the answers.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            FindValues circle = new FindValues();
+            double area = circle.FindArea(radius);
+            double cir = circle.FindCir(radius);
+
+            Console.WriteLine($"Area : {area} and the Circumferance : {cir} of the circle");
+            Console.ReadLine();
+        }
+    }
+}
