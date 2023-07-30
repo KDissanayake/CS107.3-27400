f6d0f9395a9299da88d86d0446b1168005555f6e
KDissanayake <118176408+KDissanayake@users.noreply.github.com>
2023-07-31 02:02:52 +0530

    Add files via upload

---
+++ C#Lab10.1/Calculation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Lab10._1
+{
+    internal class Calculation
+    {
+        private double number1;
+        private double number2;
+
+        public void setValues(double num1,double num2)
+        {
+            number1 = num1;
+            number2 = num2;
+        }
+
+        public double getadd()
+        {
+            return number1 + number2;
+        }
+        public double getsub()
+        {
+            return number1 - number2;
+        }
+        public double getmulti()
+        {
+            return number1 + number2;
+        }
+        public double getdiv()
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ C#Lab10.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Lab10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number 1: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a number 2: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Calculation calObj = new Calculation();
+
+            calObj.setValues(num1, num2);
+
+            Console.WriteLine("Sum : " + calObj.getadd());
+            Console.WriteLine("Sub : " + calObj.getsub());
+            Console.WriteLine("Multi : " + calObj.getmulti());
+            Console.WriteLine("Div : " + calObj.getdiv());
+        }
+    }
+}
