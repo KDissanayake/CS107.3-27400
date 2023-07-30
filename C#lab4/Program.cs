fa9802e0b3738536cdb5704e62039b6107eff338
KDissanayake <118176408+KDissanayake@users.noreply.github.com>
2023-07-31 01:20:56 +0530

    Add files via upload

---
+++ C#lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            convertvalues converter = new convertvalues();
+            converter.KilometerToMeter();
+            Console.ReadLine();
+
+
+        }
+
+    }
+ 
+}
---
+++ C#lab4/convertvalues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_lab4
+{
+    internal class convertvalues
+    {
+        public void KilometerToMeter()
+        {
+            Console.Write("Enter the values in kilometers");
+            double kilometers = Convert.ToDouble(Console.ReadLine());
+
+            double meters = kilometers * 1000;
+
+            Console.WriteLine($"{kilometers} equal to {meters} meters");
+            
+
+        }
+    }
+}
