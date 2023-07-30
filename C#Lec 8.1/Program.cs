3bf2c7d88609669ebaf01baf1208e695a390c40a
KDissanayake <118176408+KDissanayake@users.noreply.github.com>
2023-07-31 01:59:38 +0530

    Add files via upload

---
+++ C#Lec 8.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Lec_8._1
+{
+    internal class Program
+    {
+        //two dimenstional array
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
